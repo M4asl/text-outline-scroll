6f8046bec4625b6974aba405bbd1de7eb3d596bc
M4asl <mateusz.maslowiec98@gmail.com>
2022-11-14 20:27:53 +0100

    Add classes to text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 
 const imageUrl =
   'https://images.unsplash.com/photo-1668197819665-e6d3bc7c4f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
@@ -15,9 +15,13 @@ function App() {
       <img src={imageUrl} alt="Mountain" />
 
       <div className="text-parent">
-        <h1>The strength of the team is each individual member</h1>
+        <h1 className="outline">
+          The strength of the team is each individual member
+        </h1>
 
-        <h1>I really get motivated when I have doubters</h1>
+        <h1 className="outline">
+          I really get motivated when I have doubters
+        </h1>
       </div>
     </div>
   );
