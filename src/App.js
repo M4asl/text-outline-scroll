159556f228b62989b24edaa1203a6091187d4cc4
M4asl <mateusz.maslowiec98@gmail.com>
2022-11-14 20:18:48 +0100

    Create layout of page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const imageUrl =
+  'https://images.unsplash.com/photo-1668197819665-e6d3bc7c4f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-wrapper">
+      <div className="text-parent">
+        <h1>The strength of the team is each individual member</h1>
+
+        <h1>I really get motivated when I have doubters</h1>
+      </div>
+
+      <img src={imageUrl} alt="Mountain" />
+
+      <div className="text-parent">
+        <h1>The strength of the team is each individual member</h1>
+
+        <h1>I really get motivated when I have doubters</h1>
+      </div>
     </div>
   );
 }
